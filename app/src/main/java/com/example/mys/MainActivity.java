68ac969f1d35e64ea8f62adb88f486e61e507dd6
ShaniSchechter <133987037+ShaniSchechter@users.noreply.github.com>
2023-09-08 09:55:58 +0300

    first pro 1.0.0
    ניסון ראשון

---
+++ app/src/main/java/com/example/mys/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mys;
+
+itipackage com.example.mys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
